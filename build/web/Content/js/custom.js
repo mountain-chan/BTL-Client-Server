f00bfdffc1d180247ec4edd972ab825d84e7f073
DesertFoxtes <dersertfoxtes@gmail.com>
2020-06-04 11:55:28 +0700

    udpate

--- build/web/Content/js/custom.js
+++ build/web/Content/js/custom.js
@@ -3,13 +3,14 @@ $(document).ready(function () {
     $("#update_infor").on('click', 'input', function () {
         $(this).next().remove("div[class*='ui-message']");
     });
-
     $("#dangky").on('click', 'input', function () {
         $(this).next().remove("div[class*='ui-message']");
     });
+
     $("body").on('click', 'input', function () {
         $(this).next().remove("div[class*='ui-message']");
     });
+
 });
 
 
